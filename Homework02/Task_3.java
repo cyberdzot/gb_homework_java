919054e99db413618c89c36a424b22bbbaa44c6f
Vovqa <vovan102vb@gmail.com>
2023-02-19 17:47:14 +0300

    Added task #3 for HW02

---
+++ Homework02/Task_3.java
@@ -0,0 +1,27 @@
+package Homework02;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        clearConsole();
+
+        String my_str[] =  new String[]{"шалаш", "summus", "шалащ"};
+
+        for (String value : my_str)
+            System.out.println(value + " | " + isPalindrom(value));
+    }
+
+    // палиндром ли?
+    private static boolean isPalindrom(String text) {
+        int len = text.length();
+        for (int i = 0; i < len / 2; i++)
+            if (text.charAt(i) != text.charAt(len - 1 - i))
+                return false;
+
+        return true;
+    }
+
+    // чистим консоль, раудем глаз :)
+    private static void clearConsole() {
+        System.out.print("\033[H\033[J");
+    }
+}
