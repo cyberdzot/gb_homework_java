61720a399082f961261f8a549947b16a2d6fed2f
Vovqa <vovan102vb@gmail.com>
2023-02-26 17:03:55 +0300

    Fixed task #2 for HW04

--- Homework04/Task_2.java
+++ Homework04/Task_2.java
@@ -3,6 +3,11 @@ package Homework04;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+// * Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди,
+// dequeue() - возвращает первый элемент из очереди и удаляет его,
+// first() - возвращает первый элемент из очереди, не удаляя.
+
 public class Task_2 {
     public static void main(String[] args) {
         engineUI();
